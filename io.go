42309cb3a9ecf8521944a06980c1cdf4d7b4213e
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-03-05 13:20:26 +0100

    Fix no-out-link doesn't exist anymore
    Signed-off-by: William Phetsinorath <william.phetsinorath@shikanime.studio>
    Change-Id: I61286da8516cdbbe1881abd7080dcd066a6a6964
    ghstack-source-id: 4befba24e90599d5160e788e0250c249b407656b
    Pull-Request: https://github.com/shikanime/nix-sakffold/pull/33

--- io.go
+++ io.go
@@ -281,7 +281,7 @@ func buildImage(
 
 	args := []string{"build"}
 	if o.acceptFlakeConfig {
-		args = append(args, "--accept-flake-config", "--no-out-link")
+		args = append(args, "--accept-flake-config", "--no-link")
 	}
 	args = append(args, "--json", url)
 	cmd := exec.CommandContext(ctx, "nix", args...)
